151a8b2f47195fe95e3851501497067e2777a47d
sandracl72 <sandra.carrascol@uah.es>
2022-01-16 12:13:37 +0100

    Add client advanced

--- flower/client_isic.py
+++ flower/client_isic.py
@@ -37,6 +37,195 @@ seed_everything(seed)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
+class CifarClient(fl.client.NumPyClient):
+    """Flower client implementing melanoma classification using PyTorch."""
+
+    def __init__(
+        self,
+        model: Net,
+        trainloader: torch.utils.data.DataLoader,
+        testloader: torch.utils.data.DataLoader,
+        num_examples: Dict,
+    ) -> None:
+        self.model = model
+        self.trainloader = trainloader
+        self.testloader = testloader
+        self.num_examples = num_examples
+
+    def get_parameters(self) -> List[np.ndarray]:
+        # Return model parameters as a list of NumPy ndarrays
+        return [val.cpu().numpy() for _, val in self.model.state_dict().items() if 'bn' not in name]
+
+    def set_parameters(self, parameters: List[np.ndarray]) -> None:
+        # Set model parameters from a list of NumPy ndarrays
+        keys = [k for k in self.model.state_dict().keys() if 'bn' not in k]
+        params_dict = zip(keys, parameters)
+        state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+        self.model.load_state_dict(state_dict, strict=True)
+
+    def fit(
+        self, parameters: List[np.ndarray], config: Dict[str, str]
+    ) -> Tuple[List[np.ndarray], int, Dict]:
+        # Set model parameters, train model, return updated model parameters
+        self.set_parameters(parameters)
+        train(self.model, self.trainloader, self.testloader, epochs=1, es_patience=3)
+        return self.get_parameters(), self.num_examples["trainset"], {}
+
+    def evaluate(
+        self, parameters: List[np.ndarray], config: Dict[str, str]
+    ) -> Tuple[float, int, Dict]:
+        # Set model parameters, evaluate model on local test dataset, return result
+        self.set_parameters(parameters)
+        loss, auc, accuracy, f1 = val(self.model, self.testloader)
+        return float(loss), self.num_examples["testset"], {"accuracy": float(accuracy), "auc": float(auc)}
+
+
+def train(model, train_loader, validate_loader,  epochs = 10, es_patience = 3):
+    # Training model
+    print('Starts training...')
+
+    best_val = 0
+    criterion = nn.BCEWithLogitsLoss()
+    # Optimizer (gradient descent):
+    optimizer = optim.Adam(model.parameters(), lr=0.0005) 
+    # Scheduler
+    scheduler = ReduceLROnPlateau(optimizer=optimizer, mode='max', patience=1, verbose=True, factor=0.2)
+
+    patience = es_patience 
+    model.to(device)
+
+    for e in range(epochs):
+        correct = 0
+        running_loss = 0
+        model.train()
+        
+        for i, (images, labels) in enumerate(train_loader):
+
+            images, labels = images.to(device), labels.to(device)
+                
+            optimizer.zero_grad()
+            
+            output = model(images) 
+            loss = criterion(output, labels.view(-1,1))  
+            loss.backward()
+            optimizer.step()
+            
+            # Training loss
+            running_loss += loss.item()
+
+            # Number of correct training predictions and training accuracy
+            train_preds = torch.round(torch.sigmoid(output))
+                
+            correct += (train_preds.cpu() == labels.cpu().unsqueeze(1)).sum().item()
+            
+            if i % args.log_interval == 0: 
+                wandb.log({'loss': loss})
+                            
+        train_acc = correct / len(training_dataset)
+
+        val_loss, val_auc_score, val_accuracy, val_f1 = val(model, validate_loader, criterion)
+            
+        print("Epoch: {}/{}.. ".format(e+1, epochs),
+            "Training Loss: {:.3f}.. ".format(running_loss/len(train_loader)),
+            "Training Accuracy: {:.3f}..".format(train_acc),
+            "Validation Loss: {:.3f}.. ".format(val_loss/len(validate_loader)),
+            "Validation Accuracy: {:.3f}".format(val_accuracy),
+            "Validation AUC Score: {:.3f}".format(val_auc_score),
+            "Validation F1 Score: {:.3f}".format(val_f1))
+            
+        wandb.log({'Training acc': train_acc, 'training_loss': running_loss/len(train_loader),
+                    'Validation AUC Score': val_auc_score, 'Validation Acc': val_accuracy, 'Validation Loss': val_loss})
+
+        scheduler.step(val_auc_score)
+                
+        if val_auc_score > best_val:
+            best_val = val_auc_score
+            patience = es_patience  # Resetting patience since we have new best validation accuracy
+            model_path = os.path.join(f'./melanoma_fl_model_{best_val:.4f}.pth')
+            torch.save(model.state_dict(), model_path)  # Saving current best model
+            print(f'Saving model in {model_path}')
+        else:
+            patience -= 1
+            if patience == 0:
+                print('Early stopping. Best Val f1: {:.3f}'.format(best_val))
+                break
+
+    del train_loader, validate_loader, images 
+
+    return model
+                
+def val(model, validate_loader, criterion = nn.BCEWithLogitsLoss()):          
+    model.eval()
+    preds=[]            
+    all_labels=[]
+    criterion = nn.BCEWithLogitsLoss()
+    # Turning off gradients for validation, saves memory and computations
+    with torch.no_grad():
+        
+        val_loss = 0 
+    
+        for val_images, val_labels in validate_loader:
+        
+            val_images, val_labels = val_images.to(device), val_labels.to(device)
+        
+            val_output = model(val_images)
+            val_loss += (criterion(val_output, val_labels.view(-1,1))).item() 
+            val_pred = torch.sigmoid(val_output)
+            
+            preds.append(val_pred.cpu())
+            all_labels.append(val_labels.cpu())
+        pred=np.vstack(preds).ravel()
+        pred2 = torch.tensor(pred)
+        val_gt = np.concatenate(all_labels)
+        val_gt2 = torch.tensor(val_gt)
+            
+        val_accuracy = accuracy_score(val_gt2, torch.round(pred2))
+        val_auc_score = roc_auc_score(val_gt, pred)
+        val_f1_score = f1_score(val_gt, np.round(pred))
+
+        return val_loss, val_auc_score, val_accuracy, val_f1_score
+
+def test(model, test_loader):
+    test_preds=[]
+    all_labels=[]
+    with torch.no_grad():
+        
+        for _, (test_images, test_labels) in enumerate(test_loader):
+            
+            test_images, test_labels = test_images.to(device), test_labels.to(device)
+            
+            test_output = model(test_images)
+            test_pred = torch.sigmoid(test_output)
+                
+            test_preds.append(test_pred.cpu())
+            all_labels.append(test_labels.cpu())
+            
+        test_pred=np.vstack(test_preds).ravel()
+        test_pred2 = torch.tensor(test_pred)
+        test_gt = np.concatenate(all_labels)
+        test_gt2 = torch.tensor(test_gt)
+        try:
+            test_accuracy = accuracy_score(test_gt2.cpu(), torch.round(test_pred2))
+            test_auc_score = roc_auc_score(test_gt, test_pred)
+            test_f1_score = f1_score(test_gt, np.round(test_pred))
+        except:
+            test_auc_score = 0
+            test_f1_score = 0
+            pass
+
+        wandb.log({"roc": wandb.plot.roc_curve(test_gt2, test_pred2)})    
+        wandb.log({"pr": wandb.plot.pr_curve(test_gt2, test_pred2)})
+        
+        cm = wandb.plot.confusion_matrix(
+            y_true=test_gt2,
+            preds=test_pred2,
+            class_names=["benign", "melanoma"])  
+        wandb.log({"conf_mat": cm})
+
+    print("Test Accuracy: {:.5f}, ROC_AUC_score: {:.5f}, F1 score: {:.4f}".format(test_accuracy, test_auc_score, test_f1_score))  
+
+    return test_pred, test_gt, test_accuracy
+
 
 if __name__ == "__main__":
     parser = ArgumentParser() 
---
+++ flower/server_advanced.py
@@ -0,0 +1,90 @@
+import flwr as fl 
+import sys
+sys.path.append('/workspace/stylegan2-ada-pytorch')
+from melanoma_cnn_efficientnet import Net 
+from efficientnet_pytorch import EfficientNet
+
+
+def get_eval_fn(model):
+    """Return an evaluation function for server-side evaluation."""
+
+    # Load data and model here to avoid the overhead of doing it in `evaluate` itself
+    (x_train, y_train), _ = tf.keras.datasets.cifar10.load_data()
+
+    # Use the last 5k training examples as a validation set
+    x_val, y_val = x_train[45000:50000], y_train[45000:50000]
+
+    # The `evaluate` function will be called after every round
+    def evaluate(weights: fl.common.Weights) -> Optional[Tuple[float, float]]:
+        model.set_weights(weights)  # Update model with the latest parameters
+        loss, accuracy = model.evaluate(x_val, y_val)
+        return loss, accuracy
+
+    return evaluate
+
+
+def get_eval_fn(model):
+    """Return an evaluation function for server-side evaluation."""
+
+    # Load data and model here to avoid the overhead of doing it in `evaluate` itself
+    (x_train, y_train), _ = tf.keras.datasets.cifar10.load_data()
+
+    # Use the last 5k training examples as a validation set
+    x_val, y_val = x_train[45000:50000], y_train[45000:50000]
+
+    # The `evaluate` function will be called after every round
+    def evaluate(
+        weights: fl.common.Weights,
+    ) -> Optional[Tuple[float, Dict[str, fl.common.Scalar]]]:
+        model.set_weights(weights)  # Update model with the latest parameters
+        loss, accuracy = model.evaluate(x_val, y_val)
+        return loss, {"accuracy": accuracy}
+
+    return evaluate
+
+
+def fit_config(rnd: int):
+    """Return training configuration dict for each round.
+    Keep batch size fixed at 32, perform two rounds of training with one
+    local epoch, increase to two local epochs afterwards.
+    """
+    config = {
+        "batch_size": 32,
+        "local_epochs": 1 if rnd < 2 else 2,
+    }
+    return config
+
+
+def evaluate_config(rnd: int):
+    """Return evaluation configuration dict for each round.
+    Perform five local evaluation steps on each client (i.e., use five
+    batches) during rounds one to three, then increase to ten local
+    evaluation steps.
+    """
+    val_steps = 5 if rnd < 4 else 10
+    return {"val_steps": val_steps}
+
+
+
+if __name__ == "__main__":
+
+    # Load model for
+        # 1. server-side parameter initialization
+        # 2. server-side parameter evaluation
+    arch = EfficientNet.from_pretrained('efficientnet-b2')  
+    model = Net(arch=arch).to('cuda:0')
+    
+    # Create strategy
+    strategy = fl.server.strategy.FedAvg(
+        fraction_fit=1,
+        fraction_eval=1,
+        min_fit_clients=2,
+        min_eval_clients=2,
+        min_available_clients=2,
+        eval_fn=get_eval_fn(model),
+        on_fit_config_fn=fit_config,
+        on_evaluate_config_fn=evaluate_config,
+        initial_parameters=None,  # TODO
+    )
+
+    fl.server.start_server("0.0.0.0:8080", config={"num_rounds": 5}, strategy=strategy)
